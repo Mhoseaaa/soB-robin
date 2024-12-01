29bdc60fa8ff9425d0caa06a69f4051351f47d64
Tohrubo <michael.chandra@ti.ukdw.ac.id>
2024-12-01 20:06:57 +0700

    Additions and Updates (Not Finished)

--- robin.py
+++ robin.py
@@ -4,22 +4,29 @@ class RobinPreemptive:
 
 ### RAW CODE
 
+# Empty list checker
+def lst_check(lst):
+    return len(lst) == 0
+
 ## NON PREEMPTIVE
 
 # Amount of Process
 nP = int(input("How many Process do you need?: "))
 # Quantum Time
-qT = int(input("What is the Quantum Time?: "))
+qTi = int(input("What is the Quantum Time?: "))
 
 # Process, Burst and Arrival Time list
 Plist = []
 Blist = []
 Alist = []
 
+pendingP = []
+pendingB = []
+
 # Arrival + Burst Time
 for i in range(nP):
-    aT = input(f"Project {i+1} Arrival time: ")
-    bT = input(f"Project {i+1} Burst time: ")
+    aT = int(input(f"Project {i+1} Arrival time: "))
+    bT = int(input(f"Project {i+1} Burst time: "))
     
     Plist.append(f"P{i+1}")
     Alist.append(aT)
@@ -31,11 +38,18 @@ TotT = sum(Blist)
 # Time Elapsed
 TE = 0
 proc = False
-while TE < TotT:
-    if TE >= Alist[0] and proc != True:
+qT = qTi
+while len(Plist) > 0:
+    if not lst_check(Alist) and TE >= Alist[0] and proc != True:
         print(f"Process {Plist[0]} is being processed at {TE}")
         Alist.pop(0)
         proc = True
+    
+    if len(pendingP) > 0 and proc != True:
+        proc = True
+        Plist.insert(0, pendingP.pop(0))
+        Blist.insert(0, pendingB.pop(0))
+        
     elif proc:
         Blist[0] -= 1
         qT -= 1
@@ -44,5 +58,12 @@ while TE < TotT:
             print(f"Process {Plist.pop(0)} is done at {TE}")
             Blist.pop(0)
             proc = False
-    
+            qT = qTi
+            
+        elif qT == 0:
+            pendingB.append(Blist.pop(0))
+            pendingP.append(Plist.pop(0))
+            proc = False
+            qT = qTi
+            
     TE += 1
