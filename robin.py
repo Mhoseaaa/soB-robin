c4c029b8ff947acdf0f0207cb778b259056c8613
Tohrubo <michael.chandra@ti.ukdw.ac.id>
2024-12-02 20:40:48 +0700

    Finish?

--- robin.py
+++ robin.py
@@ -1,14 +1,6 @@
-class RobinPreemptive:
-    pass
-    
-
-### RAW CODE
-
 # Empty list checker
-def lst_check(lst):
+def is_empty(lst):
     return len(lst) == 0
-
-## NON PREEMPTIVE
 
 # Amount of Process
 nP = int(input("How many Process do you need?: "))
@@ -20,8 +12,16 @@ Plist = []
 Blist = []
 Alist = []
 
+# Turn-around Time and Waiting Time list
+TaT = []
+WT = []
+# Waiting Time Index
+wti = 0
+
+# Pending Processes
 pendingP = []
 pendingB = []
+pendingA = []
 
 # Arrival + Burst Time
 for i in range(nP):
@@ -32,17 +32,19 @@ for i in range(nP):
     Alist.append(aT)
     Blist.append(bT)
 
-# Total Burst Time
-TotT = sum(Blist)
-
 # Time Elapsed
 TE = 0
+# Process Checker
 proc = False
+# Quantum Time setter
 qT = qTi
-while len(Plist) > 0:
-    if not lst_check(Alist) and TE >= Alist[0] and proc != True:
+# default Burst Time
+dbt = Blist.copy()
+
+while len(Plist) > 0 or len(pendingP) > 0:
+    if not is_empty(Alist) and TE >= Alist[0] and proc != True:
         print(f"Process {Plist[0]} is being processed at {TE}")
-        Alist.pop(0)
+        pendingA.append(Alist.pop(0))
         proc = True
     
     if len(pendingP) > 0 and proc != True:
@@ -55,15 +57,34 @@ while len(Plist) > 0:
         qT -= 1
         
         if Blist[0] == 0:
-            print(f"Process {Plist.pop(0)} is done at {TE}")
+            print(f"Process {Plist.pop(0)} is done at {TE+1}")
             Blist.pop(0)
-            proc = False
             qT = qTi
             
+            TaT.append((TE+1) - pendingA.pop(0))
+            WT.append(TaT[wti] - dbt.pop(0))
+            wti += 1
+            
+            if is_empty(pendingP):
+                proc = False
+            else:
+                Plist.insert(0, pendingP.pop(0))
+                Blist.insert(0, pendingB.pop(0))
+                       
         elif qT == 0:
-            pendingB.append(Blist.pop(0))
-            pendingP.append(Plist.pop(0))
-            proc = False
-            qT = qTi
+            if len(Plist) < 2:
+                qT = qTi
+            else:
+                pendingB.append(Blist.pop(0))
+                pendingP.append(Plist.pop(0))
+                proc = False
+                qT = qTi
             
     TE += 1
+# Average Turn-Around Time
+aTaT = round(sum(TaT) / len(TaT), 2)
+# Average Waiting Time
+aWT = round(sum(WT) / len(WT), 2)
+
+print(f'Average Turn-around Time: {aTaT}')
+print(f'Average Waiting Time: {aWT}')
